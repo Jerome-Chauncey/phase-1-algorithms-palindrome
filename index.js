f3656940e99bfd97a3f3a4e8be20c0a3f4c7367f
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-23 20:10:37 +0300

    implements problem solving approach

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function isPalindrome(word) {
+  //reverse the string
+  let reversedString = word.split("").reverse().join("");
+
+  //compare original and reversed string
+
+  return word === reversedString
   // Write your algorithm here
+  // I need to wirte  a function that checks if a given string readsthe same
+  //fowards and backwards
+
+  //inputs eg. isPalindrome("racecar")= true
+  //is Palindrome("hello")= false
+  //input is a lowercase string with only letters
 }
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("a"));
+console.log(isPalindrome("robot"));
+console.log(isPalindrome("ab"));
 
 /* 
   Add your pseudocode here
-*/
+*/ //reverse the input string
+//compare the original string with the reversed string
+//if they match return true; otherwise, return false
 
 /*
   Add written explanation of your solution here
